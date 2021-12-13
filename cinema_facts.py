e3f1fc08046ce1983d9554d40616fd917e399d18
Andy kw <andy.h.kwok@pm.me>
2021-12-13 12:22:23 +0100

    adding statistics and arguments options in the script plus mock data for unit test and last requirements file

--- cinema_facts.py
+++ cinema_facts.py
@@ -3,6 +3,7 @@
 import requests
 import pandas as pd
 import sqlite3
+import argparse
 
 from zipfile import ZipFile
 
@@ -51,9 +52,8 @@ def down_and_unzip(url, zipname, finalfile):
     with ZipFile(zipname, "r") as zipObj:
         zipObj.extract(finalfile)
 
-
 def choose_cols_marque(csv_file):
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file,sep=";")
     df_no_nan = df.dropna(subset=["marque"])
     marque_dict = {}
     for idx_marque, marque in enumerate(df_no_nan["marque"].unique()):
@@ -62,20 +62,20 @@ def choose_cols_marque(csv_file):
 
 
 def choose_cols_town(csv_file):
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file,sep=";")
     depts_list = df[["com_insee", "com_nom"]].values.tolist()
     return depts_list
 
 
 def choose_cols_theaters(csv_file, networks):
-    df = pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file,sep=";")
     theater_dict = {}
     for idx_osm, osm_id in enumerate(df["osm_id"]):
         theater_dict[idx_osm] = [osm_id]
     for idx_nbscreens, nbscreeens in enumerate(df["nb_screens"]):
         theater_dict[idx_nbscreens].append(nbscreeens)
     for idx_seats, seats in enumerate(df["capacity"]):
-        theater_dict[idx_seats].append(int(seats))
+        theater_dict[idx_seats].append(seats)
     for idx_town, town_name in enumerate(df["com_nom"]):
         theater_dict[idx_town].append(town_name)
     for idx_marque, marque_name in enumerate(df["marque"]):
@@ -85,7 +85,9 @@ def choose_cols_theaters(csv_file, networks):
     for keys_theaters, vals_theaters in theater_dict.items():
         for keys_networks, vals_networks in networks.items():
             if vals_networks in vals_theaters:
-                theater_dict[keys_theaters][vals_theaters.index(vals_networks)] = keys_networks
+                theater_dict[keys_theaters][vals_theaters.index(vals_networks)] = str(
+                    keys_networks
+                )
     return theater_dict
 
 
@@ -141,35 +143,39 @@ def show_me_min_max_stats(min_max_input, dbfile):
         cur.execute(
             "SELECT osm_id,com_nom,MIN(seats) FROM theaters WHERE seats IS NOT NULL GROUP BY seats LIMIT 1"
         )
-        print('La salle ayant le moins de sièges est la suivante')
+        print("The theater with the smallest number of seats is the following:")
         print(cur.fetchall())
     elif val_switch == "max":
         cur.execute(
             "SELECT osm_id,com_nom,MAX(seats) FROM theaters WHERE seats IS NOT NULL GROUP BY seats ORDER BY seats DESC limit 1"
         )
+        print("The theater with the largest number of seats is the following:")
         print(cur.fetchall())
-        print('La salle ayant le plus de sièges est la suivante')
     con.close()
 
-show_me_min_max_stats
 
 if __name__ == "__main__":
-    down_and_unzip(  # put arg
-        "https://www.data.gouv.fr/fr/datasets/r/1d8c2bc0-d769-433a-b8ab-0f49341d4b8a",
-        "movie.zip",
-        "data.csv",
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--download",
+        nargs=3,
+        type=str,
+        help="Required: url zip_filename csv_filename example: 'https://www.data.gouv.fr/fr/datasets/r/1d8c2bc0-d769-433a-b8ab-0f49341d4b8a' 'movie.zip' 'data.csv'",
     )
-    transform_and_load_db("data.csv", "paylead.db")  # put arg
-    show_me_min_max_stats(min_max_input, dbfile)
-
-
-"""
-creating_tables("paylead.db")
-
-insert_into_db(
-    choose_cols_marque("data.csv"),
-    choose_cols_town("data.csv"),
-    choose_cols_theaters("data.csv", choose_cols_marque("data.csv")),
-    "paylead.db",
-)
-"""
+    parser.add_argument(
+        "--transform", type=str, nargs=2, help="Required: csv_filename db_filename example 'data.csv' 'paylead.db'"
+    )
+    parser.add_argument(
+        "--stats", type=str, nargs=2, help="Required: csv_filename db_filename example 'min' 'paylead.db'"
+    )
+    args = parser.parse_args()
+    if args.download:
+        url, zip_filename, csv_filename = args.download
+        down_and_unzip(url, zip_filename, csv_filename)
+    elif args.transform:
+        csv_filename, db_filename = args.transform
+        transform_and_load_db(csv_filename, db_filename)
+    elif args.stats:
+        csv_filename, db_filename = args.stats
+        show_me_min_max_stats(csv_filename, db_filename)
